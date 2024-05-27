b9b807436dc85c1e11677b7e03fbd58660b930a5
Morenax041 <168491721+Morenax041@users.noreply.github.com>
2024-05-27 20:44:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-
+let sliderElement = document. query Selector ("#slider");
+let buttonElement = document.query Selector ("#button");
+let sizePassword = document. querySelector ("#valor");
+let password = document.query Selector ("#password");
+et containerPassword = document.query Selector ("#container-password");
+let charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!';
+let novaSenha = ";
+sizePassword.innerHTML = sliderElement.value;
+slider.oninput = function() {
+sizePassword.innerHTML = this.value;
+}
+    function generatePassword()
+let pass = ";
+for(let i = 0, n = charset.length; i < sliderElement.value; ++iX
+pass += charset.charAt(Math.floor(Math.random() * n));
+｝
+console.log(pass)
+containerPassword.classList.remove("hide");
+password.innerHTML = pass;
+novaSenha = pass;
+}
+  function copyPassword()
+alert("Senha copiada com sucesso!")
+navigator.clipboard write Text(novaSenha);
+}
